4fed92ac6eb24e625020ddbb59c93a6577897d74
Paweł <pawelrzepka563@gmail.com>
2025-03-24 19:18:38 +0100

    Add new commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,27 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import sanityIntegration from "@sanity/astro"
+import { defineConfig } from "astro/config"
+import react from "@astrojs/react"
+import { loadEnv } from "vite"
+import netlify from "@astrojs/netlify"
+
+const { PUBLIC_SANITY_STUDIO_PROJECT_ID, PUBLIC_SANITY_STUDIO_DATASET } = loadEnv(
+	process.env.NODE_ENV,
+	process.cwd(),
+	""
+)
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	output: "static",
+	integrations: [
+		sanityIntegration({
+			projectId: PUBLIC_SANITY_STUDIO_PROJECT_ID,
+			dataset: PUBLIC_SANITY_STUDIO_DATASET,
+			useCdn: true,
+			studioBasePath: "/admin",
+		}),
+		react(),
+	],
+	adapter: netlify(),
+})
